56614e29b47664f00a0732b2b5cd67596d867c1b
HappyBTreeFriends <128566240+HappyBTreeFriends@users.noreply.github.com>
2023-04-13 23:03:27 +0900

    Update 2_1 ClientSession.cs

--- 2_1 ClientSession.cs
+++ 2_1 ClientSession.cs
@@ -3,11 +3,13 @@ using System.Collections.Generic;
 using System.Text;
 using System.Linq;
 using System.Net;
-using System.Net.Sockets;
 using System.Threading;
+using System.Threading.Tasks;
+using System.Net.Sockets;
 using ServerCore;
 
-namespace DummyClient
+
+namespace SServer
 {
     class Packet
     {
@@ -29,47 +31,65 @@ namespace DummyClient
     public enum PacketID
     {
         PlayerInfoReq = 1,
-        PlayerInSfoOk = 2,
+        PlayerInfoOk = 2,
     }
 
-    class ServerSession : Session
+    class ClientSession : PacketSession
     {
         public override void OnConnected(EndPoint endPoint)
         {
             Console.WriteLine($"OnConnected bytes: {endPoint}");
-            
-            PlayerInfoReq packet = new PlayerInfoReq(){
-                size = 4, packetId = (ushort)PacketID.PlayerInfoReq, playerId = 1001 };
-            
-            ArraySegment<byte> s = SendBufferHelper.Open(4096);
+
+            //Packet packet = new Packet() { size = 100, packetId = 100 };
+
+            //ArraySegment<byte> openSegment = SendBufferHelper.Open(4096);
+            //byte[] buffer = BitConverter.GetBytes(packet.size);
+            //byte[] buffer2 = BitConverter.GetBytes(packet.packetId);
+
+            //Array.Copy(buffer, 0, openSegment.Array, openSegment.Offset, buffer.Length);
+            //Array.Copy(buffer2, 0, openSegment.Array, openSegment.Offset + buffer.Length, buffer2.Length);
+            //ArraySegment<byte> sendBuff = SendBufferHelper.Close(buffer.Length + buffer2.Length);
+
+            //// 보낸다. (받기의 역순)
+            ////byte[] sendBuff = Encoding.UTF8.GetBytes("Welcom To the Jungle!");
+            //Send(sendBuff);             // 웰컴투정글 송신
+
+            Thread.Sleep(5000);                 // 1초 대기
+            Disconnect();               // 세션 연결 종료
+        }
+        // 유효한 패킷을 처리
+        public override void OnPacketRecv(ArraySegment<byte> buffer)
+        {
             ushort count = 0;
-            bool success = true;
 
-            // 혹시, 복사 실패할 경우 false 반환하여 예외처리, 주로 데이터 사이즈 문제로 false 반환.
-            success &= BitConverter.TryWriteBytes(new Span<byte>(s.Array, s.Offset, s.Count), packet.size);
+            ushort size = BitConverter.ToUInt16(buffer.Array, buffer.Offset);
             count += 2;
-            success &= BitConverter.TryWriteBytes(new Span<byte>(s.Array, s.Offset + count, s.Count - count), packet.packetId);
+            ushort id = BitConverter.ToUInt16(buffer.Array, buffer.Offset + count);
             count += 2;
-            success &= BitConverter.TryWriteBytes(new Span<byte>(s.Array, s.Offset + count, s.Count - count), packet.playerId);
-            count += 8;
 
-            ArraySegment<byte> sendbuff = SendBufferHelper.Close(count);
-            if (success)
-                Send(sendbuff);             // 송신
+            switch ((PacketID)id)
+            {
+                case PacketID.PlayerInfoReq:    //플레이어 정보 요청 패킷일때
+                    {
+                        long playerId = BitConverter.ToInt64(buffer.Array, buffer.Offset + count);  // 파싱
+                        count += 8; // 데이터 크기 누적
+                        Console.WriteLine($"PlayerInfoReq: {playerId}");    // 화면 출력
+
+                    }
+                    break;
+            }
+   
+            Console.WriteLine($"RecvPacketId: {id}, Size {size}");
         }
         public override void OnDisconnected(EndPoint endPoint)
         {
             Console.WriteLine($"OnDisconnected bytes: {endPoint}");
         }
-        public override int OnRecv(ArraySegment<byte> buffer)
-        {
-            string recvData = Encoding.UTF8.GetString(buffer.Array, buffer.Offset, buffer.Count);
-            Console.WriteLine($"[ FROM Server ] {recvData}");
-            return buffer.Count;
-        }
+
         public override void OnSend(int numOfBytes)
         {
             Console.WriteLine($"Transferred bytes: {numOfBytes}");
         }
+
     }
 }
